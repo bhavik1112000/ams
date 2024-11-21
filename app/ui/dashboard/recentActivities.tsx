f29f24859d98780cb3f1548bb95aee066cf01496
jpdoshi <jpdoshi2811@gmail.com>
2024-11-21 13:30:32 +0530

    asset-inventory: recent assets history

--- app/ui/dashboard/recentActivities.tsx
+++ app/ui/dashboard/recentActivities.tsx
@@ -1,121 +1,61 @@
-import { assets } from "@/data";
-import clsx from "clsx";
 import React, { useEffect, useState } from "react";
+import clsx from "clsx";
 
-type LogEntry = {
-  serialNo?: string;
-  date?: string;
-  status?: string;
-  issue?: string;
-  user?: string;
-  location?: string;
-  type?: string;
-  desk?: string;
-  from?: string;
-  to?: string;
-};
+import { BACKEND_URL } from "@/config";
+import moment from "moment";
 
 const RecentActivities = () => {
-  //   let recent10Logs: LogEntry[] = [];
-  const [recent10Logs, setRecent10Logs] = useState<LogEntry[]>([]);
+  const [recentLogs, setRecentLogs] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`${BACKEND_URL}/asset/recents`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const data = await response.json();
+      setRecentLogs(data.data); // Assign data to the state
+    } catch (error: any) {
+      console.error(error.message); // Handle errors
+    }
+  };
+
   useEffect(() => {
-    const transformedUserHistory = assets
-      .flatMap((asset) =>
-        asset.historyLog.userHistory.map((entry) => [
-          {
-            serialNo: asset.serialNo,
-            user: entry.user,
-            location: entry.location,
-            desk: entry.desk,
-            type: "from",
-            date: entry.from,
-          },
-          {
-            serialNo: asset.serialNo,
-            user: entry.user,
-            location: entry.location,
-            desk: entry.desk,
-            type: "to",
-            date: entry.to,
-          },
-        ])
-      )
-      .flat();
-    console.log(transformedUserHistory);
-    // const transformedMaintenanceLog = assets.flatMap((asset) =>
-    //   //     ({
-    //   //     ...asset.historyLog.maintenanceLog,
-    //   //     serialNo: asset.serialNo,
-    //   //   }))
-    //   //   .flat();
-
-    //   [...asset.historyLog.maintenanceLog]
-    // )
-
-    const transformedMaintenanceLog = assets.flatMap((asset) =>
-      asset.historyLog.maintenanceLog.map((entry) => ({
-        ...entry, // Spread existing properties of each log entry
-        serialNo: asset.serialNo, // Add serialNo to each entry
-      }))
-    );
-    // console.log(allLogs);
-    console.log(transformedMaintenanceLog);
-    const allLogs: LogEntry[] = [
-      ...transformedUserHistory,
-      ...transformedMaintenanceLog,
-    ];
-    const sorted10Logs = allLogs
-      .filter((entry) => entry.date)
-      .sort((a, b) => {
-        if (a.date && b.date) {
-          const dateA = new Date(a.date.split("-").reverse().join("-")); // Convert 'dd-mm-yyyy' to 'yyyy-mm-dd'
-          const dateB = new Date(b.date.split("-").reverse().join("-"));
-          return dateB.getTime() - dateA.getTime(); // Descending order
-        } else return 0;
-        // const dateA = new Date(a.date.split("-").reverse().join("-")); // Convert 'dd-mm-yyyy' to 'yyyy-mm-dd'
-        // const dateB = new Date(b.date.split("-").reverse().join("-"));
-        // return dateB - dateA; // Descending order
-      })
-      .slice(0, 10);
-    setRecent10Logs(sorted10Logs);
-    console.log(sorted10Logs);
+    fetchData();
   }, []);
 
   return (
     <div className="mt-12 rounded-md dark:bg-gray-800 dark:border-gray-700 bg-white border border-gray-200">
-      {recent10Logs.length > 0 ? ( // Check if there are logs to display
-        recent10Logs.map((log, index) => {
-          if (log.type) {
-            return (
-              <div className="flex border-b px-6 py-3  dark:border-gray-600">
-                <div className="w-[20%]">{log.date}</div>
-                <div className="w-[20%]">{log.serialNo}</div>
-                {log.type === "to" ? (
-                  <div>Deassigned from {log.user}</div>
-                ) : (
-                  <div>Assigned to {log.user}</div>
+      {recentLogs && recentLogs.length > 0 ? (
+        recentLogs.map((log, index) => {
+          return (
+            log.historyLog.maintenanceLog.length > 0 && (
+              <div className="flex border-b py-3  dark:border-gray-600 text-sm">
+                <div className="w-[100%] mx-8">{log.serialNo}</div>
+                {log.historyLog.maintenanceLog[0].date && (
+                  <div className="w-[100%]">
+                    {moment(
+                      new Date(log.historyLog.maintenanceLog[0].date)
+                    ).format("DD/MM/YYYY")}
+                  </div>
                 )}
-              </div>
-            );
-          } else {
-            return (
-              <div className="flex border-b px-6 py-3  dark:border-gray-600">
-                <div className="w-[20%]">{log.date}</div>
-                <div className="w-[20%]">{log.serialNo}</div>
+                <div className="w-[100%] mx-8">
+                  {log.brand} {log.model}
+                </div>
+                <div className="w-[100%] mx-8">{log.category}</div>
                 <div
                   className={clsx(
-                    "max-w-[90px] w-[20%] text-center bg-green-700 bg-opacity-60 rounded-full border-green-900",
+                    "max-w-[90px] w-[100%] text-center bg-green-700 bg-opacity-60 rounded-full border-green-900 h-[100%] my-auto",
                     {
-                      "bg-red-700 border-red-900": log.status === "in-repair",
+                      "bg-red-700 border-red-900": log.status === "In Repair",
                     }
                   )}
                 >
                   {log.status}
                 </div>
-                <div className="ml-6">{log.issue}</div>
               </div>
-            );
-          }
+            )
+          );
         })
       ) : (
         <p>No recent activities found.</p>
